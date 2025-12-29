fbb7223f3fde70c227136004dc9561300fb02ba0
Mikhail Kogan <mikhailkogan17@gmail.com>
2025-12-29 09:45:07 +0200

    Remove unused clients API route

--- dashboard/app/api/clients/route.ts
+++
@@ -1,8 +0,0 @@
-
-import { getAllClients } from '@/lib/client-metadata'
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  const clients = getAllClients()
-  return NextResponse.json(clients)
-}
